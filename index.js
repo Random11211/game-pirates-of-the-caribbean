c45f80fc2fce507170d2bd6b182d55e7c8ab593e
Konrad Szymański <konrad.szymanski07@gmail.com>
2018-12-15 13:34:20 +0100

    cos tam

--- index.js
+++ index.js
@@ -2,20 +2,17 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 800,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    },
     scene: {
         preload: preload,
         create: create,
-        //update: update,
+        update: update,
     }
 };
 
 var game = new Phaser.Game(config);
+var logo;
+var speed = 5;
+var emitter;
 
 var cursor;
 var mapa, layer1;
@@ -24,38 +21,49 @@ function preload()
 {
     this.load.image('logo', 'items/bomb2.png');
     // this.load.image('background', '/plansza/plaza.jpg');
-    this.load.tilemap('mapa', 'mapa.json', null, Phaser.Tilemap.TILED_JSON);
+    this.load.tilemapTiledJSON('mapa', 'mapa.json');
     this.load.image('tiles', 'plansza/plaza.png');
 }
 
 function create()
 {
     // this.add.image(400, 300, 'background');
-    map = game.add.tilemap('mapa');
-    map.addTilesetImage('plaza', 'tiles');
-    layer1 = map.createLayer('poziom1');
-    layer1.resizeWorld();
-
-    //cursor = game.input.keyboard.createCursorKeys();
+    this.map = this.make.tilemap({ key: 'mapa'});
+    let tileset = this.map.addTilesetImage('plaza', 'tiles');
+    this.map.createStaticLayer('poziom1', tileset).resizeWorld();
+    //layer1.resizeWorld();
 
     var particles = this.add.particles('red');
 
-    var emitter = particles.createEmitter({
-        speed: 50,
+    emitter = particles.createEmitter({
+        speed: 10,
         scale: { start: 1, end: 0 },
         blendMode: 'ADD'
     });
 
-    var logo = this.physics.add.image(100, 50, 'logo');
+    logo = this.physics.add.sprite(100, 50, 'logo');
 
-    logo.setVelocity(500, 150);
-    logo.setBounce(1, 1);
+    controls = {
+        right : game.input.keyboard.addKey(Phaser.Keyboard.RIGHT),
+        left : game.input.keyboard.addKey(Phaser.Keyboard.LEFT),
+        up : game.input.keyboard.addKey(Phaser.Keyboard.UP),       
+    };
+
+    logo.setVelocity(20, 20);
+    logo.setBounce(0.9, 1.1);
     logo.setCollideWorldBounds(true);
 
     emitter.startFollow(logo);
 }
 
-//function update() {
-//    logo.body.velocity.x = 100;
-//    logo.body.velocity.y = 50;
-//}
+function update() {
+    if (controls.right.isDown) {
+        logo.x += speed;
+    }
+    // if (this.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+    //     logo.x += speed;
+    // }
+    // else if (this.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+    //     logo.x -= speed;
+    // }
+}
